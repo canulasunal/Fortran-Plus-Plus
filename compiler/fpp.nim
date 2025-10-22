1b7bb8c418d1ae7188e7b85d747297e6241e7cab
canulasunal <uranium@tutanota.de>
2025-10-22 11:11:10 +0200

    add cleanup for .mod files

--- compiler/fpp.nim
+++ compiler/fpp.nim
@@ -19,9 +19,18 @@ else:
     if paramStr(1).endsWith(".fpp"):
         var document = readFile(command)
         document = parse(document)
+        var toremove: seq[string] = @[]
+
+        for item in document.split("\n"):
+            if item.startsWith("use "):
+                toremove.add(item.replace("use ", "").strip()&".mod")
+
         writeFile(paramStr(1).replace(".fpp", ".f90"), compile(document))
 
         discard execShellCmd("gfortran " & paramStr(1).replace(".fpp", ".f90") & " -o " & paramStr(1).replace(".fpp", ""))
         removeFile(paramStr(1).replace(".fpp", ".f90"))
+
+        for item in toremove:
+            removeFile(item)
     else:
         echo "Fatal error: Invalid file extension."
