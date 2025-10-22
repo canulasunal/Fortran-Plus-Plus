73d29a0cae3acd174c1de710b84352c6998bbc3a
canulasunal <uranium@tutanota.de>
2025-10-22 10:52:52 +0200

    add module support to compiler

--- compiler/compiler.nim
+++ compiler/compiler.nim
@@ -1,17 +1,36 @@
 import strutils
 
+const os_module = staticRead("../modules/os.f90")
+const strutils_module = staticRead("../modules/strutils.f90")
+
 proc compile*(content: string): string =
     var document = content.split("\n")
 
     var compiled: seq[string] = @[]
     var functions: seq[string] = @[]
 
+    for item in document:
+        if item == "use os":
+            for x in os_module.split("\n"):
+                compiled.add(x)
+
+        elif item == "use strutils":
+            for x in strutils_module.split("\n"):
+                compiled.add(x)
+
     compiled.add("program main")
+
+    for item in document:
+        if item.startsWith("use "):
+            compiled.add(item)
+
     compiled.add("implicit none")
 
     for item in document:
         if item.startsWith("#compiler#tag#infunction# "):
             functions.add(item.replace("#compiler#tag#infunction# ", ""))
+        elif item.startsWith("use "):
+            discard 1
         else:
             compiled.add(item)
     
