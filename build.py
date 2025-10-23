143fea9ad0c83b72ed5d7c4038ecff87918ffe2b
canulasunal <uranium@tutanota.de>
2025-10-23 20:13:31 +0200

    too lazy to write message

--- build.py
+++ build.py
@@ -10,3 +10,7 @@ if osx == "Windows":
     os.chdir("compiler")
     os.system('nim c -d:release --passL:"build.res" fpp.nim')
     os.remove("build.res")
+    shutil.move("fpp.exe", "..")
+    os.chdir("..")
+    os.mkdir("bin")
+    shutil.move("fpp.exe", "bin")
