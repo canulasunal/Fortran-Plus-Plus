b9d0aee95bab6482f4c7ec754c33f92c70ccd07c
canulasunal <uranium@tutanota.de>
2025-10-25 08:52:10 +0200

    switch to functional programming for nim codebase

--- compiler/fpp.nim
+++ compiler/fpp.nim
@@ -5,32 +5,36 @@ import version
 import parser
 import compiler
 
-if paramCount() < 1:
-    quit()
+proc main() =
 
-var command = paramStr(1)
+    if paramCount() < 1:
+        quit()
 
-if command == "--version" or command == "-V":
-    echo "Fortran++ Compiler Version " & version.version()
-    echo "Copyright (C) 2025 canulasunal"
-    echo "------------------------------"
+    var command = paramStr(1)
 
-else:
-    if paramStr(1).endsWith(".fpp"):
-        var document = readFile(command)
-        document = parse(document)
-        var toremove: seq[string] = @[]
+    if command == "--version" or command == "-V":
+        echo "Fortran++ Compiler Version " & version.version()
+        echo "Copyright (C) 2025 canulasunal"
+        echo "------------------------------"
 
-        for item in document.split("\n"):
-            if item.startsWith("use "):
-                toremove.add(item.replace("use ", "").strip()&".mod")
-
-        writeFile(paramStr(1).replace(".fpp", ".f90"), compile(document))
-
-        discard execShellCmd("gfortran " & paramStr(1).replace(".fpp", ".f90") & " -o " & paramStr(1).replace(".fpp", ""))
-        removeFile(paramStr(1).replace(".fpp", ".f90"))
-
-        for item in toremove:
-            removeFile(item)
     else:
-        echo "Fatal error: Invalid file extension."
+        if paramStr(1).endsWith(".fpp"):
+            var document = readFile(command)
+            document = parse(document)
+            var toremove: seq[string] = @[]
+
+            for item in document.split("\n"):
+                if item.startsWith("use "):
+                    toremove.add(item.replace("use ", "").strip()&".mod")
+
+            writeFile(paramStr(1).replace(".fpp", ".f90"), compile(document))
+
+            discard execShellCmd("gfortran " & paramStr(1).replace(".fpp", ".f90") & " -o " & paramStr(1).replace(".fpp", ""))
+            removeFile(paramStr(1).replace(".fpp", ".f90"))
+
+            for item in toremove:
+                removeFile(item)
+        else:
+            echo "Fatal error: Invalid file extension."
+
+main()
