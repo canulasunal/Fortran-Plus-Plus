55b10e1050247c32f045b81a215248cdb216b262
canulasunal <uranium@tutanota.de>
2025-10-30 11:30:56 +0100

    Add issquare function in math module

--- modules/math.f90
+++ modules/math.f90
@@ -7,4 +7,23 @@ module math
         real(kind=8) :: pi
         pi = 3.141592653589793238462643383279
     end function pi
+
+    function issquare(num)
+        integer :: num
+        integer :: issquare
+        integer :: i
+
+        do i = 1, 1000000
+            if (num/i == i) then
+                issquare = 1
+                exit
+            end if
+        end do
+
+        if (issquare == 1) then
+            issquare = 1
+        else
+            issquare = 0
+        end if
+    end function issquare
 end module math
